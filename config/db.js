08ed2dcd499e09176610ddbc45e5a73c82574984
khuongdg <159922010+khuongdg@users.noreply.github.com>
2025-04-17 13:38:27 +0700

    change to mongoose.connect

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection(process.env.MONGODB_URI).on('open', ()=> {
+const connection = mongoose.connect(process.env.MONGODB_URI).on('open', ()=> {
     console.log('MongoDB connected');
 }).on('error', (err) => {
     console.error('MongoDB connection error:', err);
