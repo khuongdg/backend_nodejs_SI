6c1655955eca77d5fcb9c5eddd70ba45006f3693
khuongdg <159922010+khuongdg@users.noreply.github.com>
2025-04-17 14:27:42 +0700

    delete test route

--- routes/user.router.js
+++ routes/user.router.js
@@ -3,10 +3,5 @@ const UserController = require('../controllers/user.controller');
 
 router.post('/registration', UserController.register);
 router.post('/login', UserController.login);
-// Test route để kiểm tra kết nối
-router.get('/', (req, res) => {
-    console.log('GET /');
-    res.send('Hello from router!');
-  });
 
 module.exports = router;
